f95dcf665cf3aeb001be0c1fa97f3b85589b5bdd
lap-581 <pavankumar@convergentechnologies.com>
2025-06-09 01:07:41 +0530

    3rd commit for test cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,5 +7,5 @@ def test_flight_info_mock():
     response = client.get("/api/flight-info?airline=6E&number=502&date=2025-06-10")
     assert response.status_code == 200
     data = response.json()
-    assert "airline" in data
-    assert data["airline"] == "6E"
+    assert "status" in data
+    assert data["status"] == "On Time"
